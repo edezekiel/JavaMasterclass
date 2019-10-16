9113aef003fafeeba91f6718ac6e3796c1a0e5b1
Ed Ezekiel <ed@ubiquisoft.com>
2019-10-16 16:23:58 -0500

    logical operators

---
+++ ConditionalLogic/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+	    boolean isAlien = false;
+	    if (isAlien == false) {
+	    	System.out.println("It is not an alien!");
+	    } else {
+	    	System.out.println("It is an alien.");
+	    }
+
+	    int topScore = 80;
+	    if (topScore >= 100) {
+	    	System.out.println("highscore");
+	    }
+
+	    int secondTopScore = 81;
+	    if ((topScore > secondTopScore) || (topScore < 100)) {
+	    	System.out.println("greater than second top score OR less than 100");
+	    }
+
+
+    }
+}
