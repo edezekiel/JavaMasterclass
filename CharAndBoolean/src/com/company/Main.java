7737994ad721b3cc03e496df5d1cc3a4733b6244
Ed Ezekiel <ed@ubiquisoft.com>
2019-10-16 15:23:02 -0500

    Char, Booleans, and Strings

---
+++ CharAndBoolean/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    	char myChar = 'D';
+    	System.out.println(myChar);
+    	char myUnicodeChar = '\u0044';
+    	System.out.println(myUnicodeChar);
+
+    	boolean myTrueBooleanValue = true;
+    	boolean myFalseBooleanValue = false;
+
+    	boolean isCustomerOverTwentyOne = true;
+    }
+}
