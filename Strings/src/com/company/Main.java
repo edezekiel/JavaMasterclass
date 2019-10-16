d77ef78b867cfeafd2e80a8451f97294fbaecfbc
Ed Ezekiel <ed@ubiquisoft.com>
2019-10-16 15:32:27 -0500

    Strings

--- Strings/src/com/company/Main.java
+++ Strings/src/com/company/Main.java
@@ -3,6 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	
+		String myString = "This is a string";
+		System.out.println("myString is equal to " + myString);
+		myString = myString + (", and this is more. " + myString);
+	    System.out.println("Updated myString is equal to " + myString);
+
+	    String numberString = "250.55";
+	    numberString = numberString +"49.95";
+	    System.out.println((numberString));
+		String lastString = "10";
+		int myInt = 50;
+		// Java converts int value to string and appends it to the string.
+		lastString = lastString + myInt;
+		System.out.println("lastString is equal to " + lastString);
+
+		double doubleNumber = 120.47d;
+		lastString = lastString + doubleNumber;
+		System.out.println(lastString);
     }
 }
