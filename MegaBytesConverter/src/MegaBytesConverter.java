c065cba87d67dbbc3d8e2fb02ffea246751ee4c6
Ed Ezekiel <ed@ubiquisoft.com>
2019-10-17 12:56:01 -0500

    Solution to MegaBytesConverter

--- MegaBytesConverter/src/MegaBytesConverter.java
+++ MegaBytesConverter/src/MegaBytesConverter.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class MegaBytesConverter {
+	public static void printMegaBytesAndKiloBytes(int kiloBytes){
+
+		int megaBytes = kiloBytes / 1024;
+		int remainder = kiloBytes % 1024;
+
+		if (kiloBytes < 0) {
+			System.out.println("Invalid Value");
+		} else {
+			System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + remainder + " KB");
+		}
+	}
 }
