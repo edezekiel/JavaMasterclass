8e8a4da8341fa486ebdf123ad771ed7a683ab17b
Ed Ezekiel <ed@ubiquisoft.com>
2019-10-17 16:01:19 -0500

    calFeet

--- MethodOverloading/src/com/company/Main.java
+++ MethodOverloading/src/com/company/Main.java
@@ -6,15 +6,40 @@ public class Main {
 		int newScore = calculateScore("Tim", 500);
 		System.out.println("New score is " + newScore);
 		calculateScore(300);
+
+		calcFeetAndInchesToCentimeters(15);
 	}
 
 	public static int calculateScore(String playerName, int score) {
-		System.out.println("Player " + playerName + " scored " + score + " points");
+		System.out.println("Player " + playerName + " scored " + score +
+				                   " points");
 		return score * 1000;
 	}
 
 	public static int calculateScore(int score) {
 		System.out.println("Unnamed player scored " + score + " points");
 		return score * 1000;
+	}
+
+	public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+		if (feet >= 0 && inches >= 0 && inches <= 12) {
+			double centimeters = (2.54 * ((feet * 12) + inches));
+			System.out.println(feet + " feet + " + inches + " inches" + " = " + centimeters + " centimeters.");
+			return centimeters;
+		} else {
+			return -1;
+		}
+	}
+
+	public static double calcFeetAndInchesToCentimeters(double inches) {
+		if (inches >= 0) {
+			// calculate how many feet are in the inches
+			System.out.println("Starting with this many inches: " + inches);
+			double feet = (int) (inches / 12);
+			double remainingInches = inches % 12;
+			// call the other overloaded method
+			calcFeetAndInchesToCentimeters(feet, remainingInches);
+		}
+		return -1;
 	}
 }
