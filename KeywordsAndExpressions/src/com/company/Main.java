7ae01d52742f4e9f7586a974b638c1d36151afdb
Ed Ezekiel <ed@ubiquisoft.com>
2019-10-17 08:50:11 -0500

    Keywords, expressions, statements, white space, indentation

---
+++ KeywordsAndExpressions/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.awt.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// This is an expression
+		int /*not including int*/ firstVariable = 20 /*not including semicolon*/;
+		System.out.println(firstVariable);
+		// These are Statements. Semi-colon is almost always needed to complete a Java statement.
+		int myVariable = 50;
+		myVariable++;
+		System.out.println("This is a complete Java statement.");
+	}
+}
