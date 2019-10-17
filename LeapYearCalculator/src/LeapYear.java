f58adad114f89247b98efe83c25138c323265412
Ed Ezekiel <ed@ubiquisoft.com>
2019-10-17 13:51:08 -0500

    Method Overloading

--- LeapYearCalculator/src/LeapYear.java
+++ LeapYearCalculator/src/LeapYear.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
 public class LeapYear {
+	public static boolean isLeapYear(int year) {
+		if (year < 1 || year > 9999) {
+			return false;
+
+			// Step 1
+		} else if (year % 4 == 0) {
+
+			// Step 2
+			if (year % 100 == 0) {
+
+				// Step 3
+				if (year % 400 == 0) {
+
+					// Step 3-4
+					return true;
+				} else {
+
+					// Step 3-5
+					return false;
+				}
+			} else {
+
+				// Step 2-4
+				return true;
+			}
+		}
+		// Step 1-5
+		return false;
+	}
 }
