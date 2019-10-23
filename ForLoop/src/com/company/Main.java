0980410b5c33f5b9b2689cedcc77cd7006fa59c1
Ed Ezekiel <ed@ubiquisoft.com>
2019-10-23 13:25:37 -0500

    rangeIsPrime

--- ForLoop/src/com/company/Main.java
+++ ForLoop/src/com/company/Main.java
@@ -8,9 +8,44 @@ public class Main {
 		    System.out.println("10,000 at " + i + "% interest = " +
 				                       String.format("%.2f", calculateInterest(10000.00, i)));
 	    }
+
+    	rangeIsPrime(10);
     }
 
     public static double calculateInterest(double amount, double interesteRate) {
     	return (amount * (interesteRate/100));
+    }
+
+    public static boolean isPrime(int n) {
+    	// 1 is not a prime number.
+    	if (n <= 1) {
+    		return false;
+	    }
+
+    	// i <= n/2 optimizes the function. If we can't find any factors less
+	    // than or equal to sqrt(n), n must be prime. I.e., if a number n is not
+	    // prime, it can be factored into two factors a and b.
+	    // a * b cannot be greater than n.
+    	for (int i=2; i <= n/2; i++) {
+    		// This would be true when a number evenly divides into n.
+		    // In that case, n would not be prime.
+    		if (n % i == 0) {
+    			return false;
+		    }
+	    }
+    	return true;
+    }
+
+    public static void rangeIsPrime(int number) {
+    	int numOfPrimes = 0;
+    	for (int i = 0; i < number; i++) {
+    		if (isPrime(i)) {
+			    numOfPrimes += 1;
+			    System.out.println("Prime found: " + i);
+			    if (numOfPrimes == 3) {
+			    	break;
+			    }
+		    }
+	    }
     }
 }
